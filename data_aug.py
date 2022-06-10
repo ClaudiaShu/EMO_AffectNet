02899b7a74dab75a2fe624e937c477f8958f9b71
Yuxuan Shu <48800547+ClaudiaShu@users.noreply.github.com>
2022-06-10 10:37:30 +0100

    Update data_aug.py

--- data_aug.py
+++ data_aug.py
@@ -50,6 +50,7 @@ def get_simclr_pipeline_transform(size, s=1):
 def get_image_transform(size=112):
     train_transform = cv_tf.Compose(
         [
+            # cv_tf.ToPILImage(),
             imgRandomLandmarkMask(),
             cv_tf.Resize(size=(112, 112)),
             # cv_tf.RandomCrop(112),
@@ -75,6 +76,40 @@ def get_image_transform(size=112):
     test_transform = cv_tf.Compose([
         # cv_tf.ToPILImage(),
         cv_tf.Resize(size=(size, size)),
+        cv_tf.ToTensor(),
+        cv_tf.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
+    return train_transform, test_transform
+
+
+def get_cv_image_transform(size=112):
+    train_transform = cv_tf.Compose(
+        [
+            cv_tf.ToPILImage(),
+            imgRandomLandmarkMask(),
+            # cv_tf.Resize(size=(112, 112)),
+            # cv_tf.RandomCrop(112),
+            cv_tf.RandomHorizontalFlip(p=0.5),
+            # transforms.RandomApply
+            # cv_tf.RandomApply([cv_tf.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.2)], p=0.8),
+            cv_tf.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.2),
+            cv_tf.RandomApply([cv_tf.GaussianBlur(3)], p=0.5),
+            # cv_tf.GaussianBlur(3),
+            cv_tf.RandomGrayscale(p=0.2),
+            cv_tf.ToTensor(),
+            cv_tf.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+        ]
+    )
+    # train_transform = cv_tf.Compose([
+    #     # cv_tf.ToPILImage(),
+    #     cv_tf.Resize(size=(size, size)),
+    #     cv_tf.RandomHorizontalFlip(),
+    #     cv_tf.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4),
+    #     # cv_tf.GaussianBlur(3), # add
+    #     cv_tf.ToTensor(),
+    #     cv_tf.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
+    test_transform = cv_tf.Compose([
+        cv_tf.ToPILImage(),
+        # cv_tf.Resize(size=(size, size)),
         cv_tf.ToTensor(),
         cv_tf.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
     return train_transform, test_transform
