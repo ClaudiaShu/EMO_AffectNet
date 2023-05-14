6bbec3fa81971e5dac599e8042d5cdf2edf2fcc5
Yuxuan Shu <48800547+ClaudiaShu@users.noreply.github.com>
2023-05-14 13:26:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ from torch.backends import cudnn
 from torch.utils.data import DataLoader
 
 from ABAW_main import ABAW_trainer
-from data_aug import get_image_transform, get_audio_transform
-from data_loader import AFFECTNET_Dataset, Aff2_Dataset_static_shuffle, Aff2_Dataset_audio
+from data_aug import get_cv_image_transform
+from data_loader import AFFECTNET_Dataset, AFFECTNET_hdf5
 from loss.balanced_softmax_cross_entropy_loss import BalancedSoftmaxCE
 from loss.class_balanced_loss import ClassBalanceCE
-from models.model_R3D_DFEW import DFEW_SSL
+from models.model_FaceCycle import Cycle_feature
+
 from models.model_RES import RES_SSL
 from models.model_RES_imagenet import RES_feature
-from utils import seed_everything, create_original_data, FocalLoss
-from utils.prefetch_dataloader import DataLoaderX
+from utils import seed_everything, create_original_data, FocalLoss, CCCLoss
 
 from config import *
 
@@ -29,23 +29,25 @@ parser.add_argument('--comment', type=str, default='')
 parser.add_argument('--lr', default=1e-4, type=float, help='learning rate')  # 1e-4#
 parser.add_argument('--batch_size', default=64, type=int, help='batch size')  # 64#
 parser.add_argument('--epochs', default=20, type=int, help='number epochs')  # 20#
-parser.add_argument('--num_classes', default=8, type=int, help='number classes')
+# parser.add_argument('--num_classes', default=8, type=int, help='number classes')
 parser.add_argument('-weight_decay', default=5e-4, type=float)  # 5e-4#
 parser.add_argument('--seq_len', default=3, type=int)
 parser.add_argument('--sec', default=1)
-parser.add_argument('--num_workers', default=12, type=int)
+parser.add_argument('--num_workers', default=4, type=int)
 parser.add_argument('--resume', type=bool, default=False)
 
 # parser.add_argument('--loss', type=str, default='CrossEntropyLoss')  # ['CrossEntropyLabelAwareSmooth','SuperContrastive']
-# parser.add_argument('--warmup', type=bool, default=False)
+# parser.add_argumenat('--warmup', type=bool, default=False)
 # parser.add_argument('--optim', type=str, default='ADAM')
 
+parser.add_argument('--task', default='exp', type=str, choices=['exp','va'])
 parser.add_argument('--arch', default='resnet50', type=str, help='baseline of the training network.')
-parser.add_argument('--rep', default='pretrain', type=str, help='Choose methods for representation learning.') # ['SSL','pretrain']
+parser.add_argument('--rep', default='SSL', type=str, help='Choose methods for representation learning.') # ['SSL','pretrain']
 parser.add_argument('--dataset', default='AffectNet') # AffectNet Aff2
 parser.add_argument('--mode', default='image') # image audio
-parser.add_argument('--model_dir', default='/mnt/d/Data/Yuxuan/logging/simCLR/VoxCeleb_img/runs/resnet50_voxceleb1s_img/checkpoint_0090.pth.tar')
+parser.add_argument('--model_dir', default="/data2/yshu21/data/model/SSL_model/simclr_with_time_neg_img_c/checkpoint_best.pth.tar")
 parser.add_argument('--state_dict', default="/mnt/d/Data/Yuxuan/downstream/image/runs/Jun07_11-20-10_ic_xiao/checkpoint_bast.pth.tar")
+parser.add_argument('--loadmodel', default='/data2/yshu21/data/FaceCycle/FaceCycleModel.tar')
 
 parser.add_argument('--gpu-index', default=0, type=int, help='Gpu index.')
 parser.add_argument('--fp16-precision', action='store_true',
@@ -55,66 +57,35 @@ parser.add_argument('--log-every-n-steps', default=100, type=int,
 
 args = parser.parse_args()
 
-def setup_Aff2(args):
-    # train set
-    df_train = create_original_data(
-        f'/mnt/c/Data/Yuxuan/ABAW/labels_save/expression/Train_Set_v3/*')
-    # valid set
-    df_valid = create_original_data(
-        f'/mnt/c/Data/Yuxuan/ABAW/labels_save/expression/Validation_Set_v3/*')
-
-    if args.mode == 'image':
-        train_transform, test_transform = get_image_transform()
-        train_dataset = Aff2_Dataset_static_shuffle(df=df_train, root=False, transform=train_transform)
-        valid_dataset = Aff2_Dataset_static_shuffle(df=df_valid, root=False, transform=test_transform)
-    elif args.mode == 'audio':
-        train_transform_aud, test_transform_aud = get_audio_transform()
-        train_dataset = Aff2_Dataset_audio(df=df_train, sec=args.sec, root=False, transform_aud=train_transform_aud)
-        valid_dataset = Aff2_Dataset_audio(df=df_valid, sec=args.sec, root=False, transform_aud=test_transform_aud)
-    else:
-        raise ValueError('invalid mode')
-
-    train_loader = DataLoader(dataset=train_dataset,
-                               batch_size=args.batch_size,
-                               num_workers=args.num_workers,
-                               pin_memory=False,
-                               shuffle=True,
-                               drop_last=False)
-    valid_loader = DataLoader(dataset=valid_dataset,
-                               batch_size=args.batch_size,
-                               num_workers=args.num_workers,
-                               pin_memory=False,
-                               shuffle=False,
-                               drop_last=False)
-
-
-    return train_loader, valid_loader
-
 def setup_AffectNet(args):
-    train_transform, valid_transform = get_image_transform()
-    # train set
-    train_data = "/mnt/c/Data/Yuxuan/AffectNet/train_set_data.csv"
-    valid_data = "/mnt/c/Data/Yuxuan/AffectNet/val_set_data.csv"
+    train_transform, valid_transform = get_cv_image_transform(size=64)
+    train_data = "/data2/ys221/data/AffectNet/train_data.csv"
+    valid_data = "/data2/ys221/data/AffectNet/val_data.csv"
 
     train_dataset = AFFECTNET_Dataset(df=train_data,
-                                root=False,
+                                task=args.task,
                                 transform=train_transform)
     valid_dataset = AFFECTNET_Dataset(df=valid_data,
-                                      root=False,
+                                      task=args.task,
                                       transform=train_transform)
-
-    train_loader = DataLoader(dataset=train_dataset,
-                              batch_size=args.batch_size,
-                              num_workers=args.num_workers,
-                              pin_memory=False,
-                              shuffle=True,
-                              drop_last=False)
-    valid_loader = DataLoader(dataset=valid_dataset,
-                              batch_size=args.batch_size,
-                              num_workers=args.num_workers,
-                              pin_memory=False,
-                              shuffle=False,
-                              drop_last=False)
+    # train_data = "/data2/yshu21/data/AffectNet/cache/AffectNet_train.hdf5"
+    # valid_data = "/data2/yshu21/data/AffectNet/cache/AffectNet_valid.hdf5"
+    #
+    # train_dataset = AFFECTNET_hdf5(train_data, transform=train_transform, task=args.task)
+    # valid_dataset = AFFECTNET_hdf5(valid_data, transform=valid_transform, task=args.task)
+    #
+    # train_loader = DataLoader(dataset=train_dataset,
+    #                           batch_size=args.batch_size,
+    #                           num_workers=args.num_workers,
+    #                           pin_memory=False,
+    #                           shuffle=True,
+    #                           drop_last=False)
+    # valid_loader = DataLoader(dataset=valid_dataset,
+    #                           batch_size=args.batch_size,
+    #                           num_workers=args.num_workers,
+    #                           pin_memory=False,
+    #                           shuffle=False,
+    #                           drop_last=False)
 
     return train_loader, valid_loader
 
@@ -130,9 +101,7 @@ def main():
     else:
         args.device = torch.device('cpu')
 
-    if args.dataset == 'Aff2':
-        train_loader, valid_loader = setup_Aff2(args)
-    elif args.dataset == 'AffectNet':
+    if args.dataset == 'AffectNet':
         train_loader, valid_loader = setup_AffectNet(args)
     else:
         raise ValueError('Invalid input of dataset')
@@ -156,17 +125,23 @@ def main():
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=num_steps, eta_min=0,
                                                            last_epoch=-1)
 
-    num_class_list = [74874, 134415, 25459, 14090, 6378, 3803, 24882, 3750]
-    para_dict = {
-        "num_classes": args.num_classes,
-        "num_class_list": num_class_list,
-        "device": device,
-        "cfg": cfg_loss,
-    }
-    # criterion = nn.CrossEntropyLoss().to(args.device)
-    # criterion = FocalLoss(args.num_classes, batch_size=args.batch_size).to(args.device)
-    criterion = ClassBalanceCE(para_dict).to(args.device)
-    # criterion = BalancedSoftmaxCE(para_dict).to(args.device)
+    if args.task == 'exp':
+        num_class_list = [74874, 134415, 25459, 14090, 6378, 3803, 24882, 3750]
+        para_dict = {
+            "num_classes": 8,
+            "num_class_list": num_class_list,
+            "device": device,
+            "cfg": cfg_loss,
+        }
+        # criterion = nn.CrossEntropyLoss().to(args.device)
+        # criterion = FocalLoss(args.num_classes, batch_size=args.batch_size).to(args.device)
+        # criterion = ClassBalanceCE(para_dict).to(args.device)
+        criterion = BalancedSoftmaxCE(para_dict).to(args.device)
+    elif args.task == 'va':
+        # criterion = nn.MSELoss()
+        criterion = CCCLoss()
+    else:
+        raise ValueError('Invalid task')
 
     optimizer.zero_grad()
     optimizer.step()
@@ -194,13 +169,13 @@ def main():
 
     #  It’s a no-op if the 'gpu_index' argument is a negative integer or None.
     with torch.cuda.device(args.gpu_index):
-        exp_train = ABAW_trainer(best_acc,
+        train = ABAW_trainer(best_acc,
                                  model=model,
                                  optimizer=optimizer,
                                  scheduler=scheduler,
                                  criterion=criterion,
                                  args=args)
-        exp_train.run(train_loader, valid_loader)
+        train.run(train_loader, valid_loader)
 
 
 if __name__ == "__main__":
